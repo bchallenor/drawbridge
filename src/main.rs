6896e34d4d2037f4ca6865cb9b5d571f1df6fc57
Ben Challenor <ben@challenor.org>
2018-01-22 07:24:27 +0000

    Allow "self" to be specified instead of IP address
    Resolved to the current public IP address using checkip.amazonaws.com.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 extern crate clap;
 #[macro_use]
 extern crate error_chain;
+extern crate futures;
+extern crate hyper;
 extern crate ipnet;
 extern crate openssl_probe;
 extern crate rusoto_core;
 extern crate rusoto_ec2;
 extern crate rusoto_route53;
+extern crate tokio_core;
 
 mod cloud;
 mod dispatch;
@@ -23,10 +26,16 @@ use dispatch::Command;
 use dispatch::dispatch;
 use dns::aws::AwsDns;
 use errors::*;
+use futures::Future;
+use futures::Stream;
+use hyper::Client;
+use hyper::StatusCode;
 use ipnet::Ipv4Net;
 use iprules::IpProtocol;
 use std::net::Ipv4Addr;
+use std::str;
 use std::str::FromStr;
+use tokio_core::reactor::Core;
 
 quick_main!(run);
 
@@ -83,9 +92,15 @@ fn run() -> Result<()> {
             .map(|x| IpProtocol::from_str(x).chain_err(|| format!("not a protocol: {}", x)))
             .collect::<Result<Vec<_>>>()?;
 
-        let ip_cidrs = matches
+        let include_own_ip_addr = matches
             .values_of("source")
             .expect("required")
+            .any(|x| x == "self");
+
+        let mut ip_cidrs = matches
+            .values_of("source")
+            .expect("required")
+            .filter(|&x| x != "self")
             .map(|x| {
                 if x.contains('/') {
                     Ipv4Net::from_str(x).chain_err(|| format!("not an IP network: {}", x))
@@ -96,6 +111,13 @@ fn run() -> Result<()> {
                 }
             })
             .collect::<Result<Vec<_>>>()?;
+
+        if include_own_ip_addr {
+            let own_ip_addr = find_own_ip_addr()?;
+            let own_ip_cidr = Ipv4Net::new(own_ip_addr, 32).expect("32 is OK");
+            println!("Substituted: self -> {}", own_ip_cidr);
+            ip_cidrs.push(own_ip_cidr);
+        }
 
         let instance_type = matches.value_of("instance-type").map(InstanceType::new);
 
@@ -121,4 +143,21 @@ fn run() -> Result<()> {
     let dns = AwsDns::new()?;
 
     dispatch(cmd, &cloud, &dns)
+}
+
+fn find_own_ip_addr() -> Result<Ipv4Addr> {
+    let mut core = Core::new().chain_err(|| "failed to create core reactor")?;
+    let client = Client::new(&core.handle());
+    let uri = "http://checkip.amazonaws.com/".parse().expect("valid URL");
+    let (status, body) = core.run(
+        client
+            .get(uri)
+            .and_then(|res| (futures::finished(res.status()), res.body().concat2())),
+    ).chain_err(|| "failed to contact checkip service")?;
+    let content = str::from_utf8(&*body).chain_err(|| "expected checkip to return UTF8")?;
+    if status != StatusCode::Ok {
+        bail!("checkip service returned {}: {}", status, content);
+    }
+    Ipv4Addr::from_str(content.trim_right())
+        .chain_err(|| format!("expected checkip to return IP address: {}", content))
 }
